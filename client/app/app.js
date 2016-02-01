498f211dfb39aadb0846f3d798c985a2a634ed4a
Stuart Hiller <hiller.stuart@gmail.com>
2016-02-01 13:41:55 -0800

    pushing up the chess application

--- client/app/app.js
+++ client/app/app.js
@@ -4,7 +4,18 @@ app.config(['$routeProvider', '$locationProvider', function($routeProvider, $loc
 	$routeProvider
 		.when(
 			'/', {
-				templateUrl: 'app/views/index.html'
+				templateUrl: 'app/views/index.html',
+				controller: function($scope) {
+					$scope.initGame = function() {
+						var board2 = ChessBoard('board1', {
+					  		draggable: true,
+					  		dropOffBoard: 'trash',
+					  		sparePieces: true
+						});
+						$('#startBtn').on('click', board1.start);
+						$('#clearBtn').on('click', board1.clear);
+					}
+				}
 			}
 		)
 		.when('/signup', {
