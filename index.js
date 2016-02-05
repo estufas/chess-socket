0725456051fe330690d9172c4eab73dbbdf614a2
Wyatt Williams <williams.wyatt@gmail.com>
2016-02-05 13:09:40 -0800

    heroku push

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ var jwt          = require('jsonwebtoken');
 var secret       = "juicyjforpresident";
 
 //Connect to mongo, then execute server logic
-// mongoose.connect( 'mongodb://production:project@ds060968.mongolab.com:60968/final');
-mongoose.connect('mongodb://localhost/final_project')
+mongoose.connect( 'mongodb://production:project@ds060968.mongolab.com:60968/final');
+// mongoose.connect('mongodb://localhost/final_project')
 mongoose.connection.once('open', function(){
 //Middleward etc.
   app.use(bodyParser.json());
